b6b121bf80cca446a83b43891b92282ea4e03f8f
Paper-Dragon <2678885646@qq.com>
2024-07-05 16:30:04 +0000

    refine code

--- container.sh
+++ container.sh
@@ -1,37 +1,35 @@
 #!/bin/bash
 
-hostname $container
+# 定义字体颜色
+Green="\033[32m"
+Red="\033[31m"
+#Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+YellowBG="\033[43;37m"
+Font="\033[0m"
 
-mkdir -p /sys/fs/cgroup/memory/$container
-echo $$ > /sys/fs/cgroup/memory/$container/cgroup.procs
-echo $memory > /sys/fs/cgroup/memory/$container/memory.limit_in_bytes
-
-mkdir -p $container/rwlayer
-mount -t aufs -o dirs=$container/rwlayer:./images/$image none $container
-
-mkdir -p $container/old_root
-cd $container
-
-mount --make-rprivate /
-
-pivot_root . ./old_root
-
-# proc
-mkdir /proc && echo "/proc create success" || echo "/proc fs maybe fail"
-mount -t proc proc /proc
-
-# /sys
-mkdir /sys && echo "/sys create success" || echo "/sys fs maybe fail"
-mount -t sysfs sys /sys
-
-# /dev
-# mount -t tmpfs tmpfs /dev
+# 定义通知信息
+Info="${Green}[信息]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[错误]${Font}"
+Warning="${Red}[警告]${Font}"
+Debug="${Green}[调试]${Font}"
 
 
+# 判断操作是否成功
+judge() {
+  if [[ 0 -eq $? ]]; then
+    echo -e "${OK} ${GreenBG} $1 完成 ${Font}"
+    sleep 0.5
+  else
+    echo -e "${Error} ${RedBG} $1 失败${Font}"
+    exit 1
+  fi
+}
 
-umount -l /old_root
-
-if test "$network" = bridge; then
+# ========================================================== #
+configure_network_bridge() {
 	while true
 	do
 		ip link show veth1-$container
@@ -45,6 +43,74 @@ if test "$network" = bridge; then
 	ip addr add $addr/24 dev veth1-$container
 	ip link set dev veth1-$container up
 	ip route add default via 172.31.0.1 dev veth1-$container
-fi
 
-exec $program
+}
+
+set_container_hostname() {
+    hostname "$container"
+    judge "给容器设置主机名为 ${container}"
+}
+
+set_container_hostname
+
+prepare_container_tor_filesystem() {
+	mkdir -p ${PWD}/$container/rwlayer
+	judge "创建rwlayer目录"
+
+	# 尝试挂载aufs文件系统
+	judge "${Debug} 尝试使用 aufs挂载"
+
+	if mount -t aufs -o dirs="$container/rwlayer:./images/$image" none "$container"; then
+        echo -e "${Debug} 使用aufs挂载成功"
+	# 尝试挂载overlay文件系统
+    else
+		echo -e "${Warning} Aufs创建失败 切换为overlay模式${Font}"
+		mkdir -p "$container/upperdir" "$container/workdir"
+        judge "创建overlay所需的upperdir和workdir目录"
+
+		mount -t overlay -o lowerdir=${PWD}/images/$image,upperdir=${PWD}/$container/upperdir,workdir=${PWD}/$container/workdir overlay $container
+		ls $container
+		if [ $? -eq 0 ] ; then
+			echo -e "${Debug} mount -t overlay -o lowerdir=${PWD}/images/$image,upperdir=${PWD}/$container/upperdir,workdir=${PWD}/$container/workdir overlay $container"
+        	echo -e "${Debug} 使用overlay挂载成功"
+    	else
+			echo -e "${Error} 挂载文件系统失败，无法使用aufs或overlay"
+			exit 1
+		fi
+    fi
+	
+}
+
+prepare_container_tor_filesystem
+
+
+mkdir -p $container/old_root
+judge "创建root fd 转换"
+
+cd $container
+mount --make-rprivate /
+judge "设置根fd私有化"
+
+pivot_root . ./old_root
+
+
+exec chroot . sh -c '
+    # 重置PATH，确保能找到基本命令
+    export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+    # 重新挂载/proc, /sys, /dev等
+    mount -t proc proc /proc
+    mount -t sysfs sys /sys
+    mount -t tmpfs tmpfs /dev
+    # 其他初始化操作...
+    # 最终执行你的程序
+    exec '"$program"'
+'
+
+# # umount -l /old_root
+# # # 网络配置示例（针对桥接模式）
+
+# # if test "$network" = bridge; then
+# # 	configure_network_bridge
+# # fi
+
+# exec $program
--- docker.sh
+++ docker.sh
@@ -1,93 +1,161 @@
 #!/bin/bash
 
+# 定义字体颜色
+Green="\033[32m"
+Red="\033[31m"
+#Yellow="\033[33m"
+GreenBG="\033[42;37m"
+RedBG="\033[41;37m"
+YellowBG="\033[43;37m"
+Font="\033[0m"
 
-usage () {
-	echo -e "\033[31mIMPORTANT: Run As Root\033[0m"
-	echo ""
-	echo "Usage:	docker.sh [OPTIONS]"
-	echo ""
-	echo "A docker written by shell"
-	echo ""
-	echo "Options:"
-	# echo "		-c string	docker command"
-	# echo "				(\"run\")"
-	echo "		-m		memory"
-	echo "				(\"100M, 200M, 300M...\")"
-	echo "		-C string	container name"
-	echo "		-I string	image name"
-	# echo "		-V string	volume [Furture]"
-	echo "		-P string	program to run in container"
-	echo "		-n string	network[host|none|bridge]"
+# 定义通知信息
+Info="${Green}[信息]${Font}"
+OK="${Green}[OK]${Font}"
+Error="${Red}[错误]${Font}"
+Warning="${Red}[警告]${Font}"
+Debug="${Green}[调试]${Font}"
 
-	return 0
+# 判断操作是否成功
+judge() {
+    if [[ 0 -eq $? ]]; then
+        echo -e "${OK} ${GreenBG} $1 完成 ${Font}"
+        sleep 0.1
+    else
+        echo -e "${Error} ${RedBG} $1 失败${Font}"
+        exit 1
+    fi
 }
 
-if test "$(whoami)" != root
-then
-	usage
-	exit -1
-fi
+# 显示使用说明
+usage() {
+    echo -e "\n${RedBG} 注意：请以root身份运行 ${Font}\n"
+    echo -e "${GreenBG}[ 用 法 ]${Font}: docker.sh [OPTIONS]"
+    
+    echo -e "\n${GreenBG}选 项:${Font}"
+    echo "-m 内存限制, 如：100M, 200M"
+    echo "-C 容器名称"
+    echo "-I 镜像名称"
+    echo "-P 容器内运行的程序"
+    echo "-n 网络模式 [host|none|bridge]"
+    
+    echo -e "\n${GreenBG} 示例:${Font}"
+    echo -e "${YellowBG}git clone --single-branch -b alpine-3.17.3  https://github.com/Paper-Dragon/docker-sh images/alpine-3.17.3${Font}"
+    echo -e "${YellowBG}docker.sh -m 100M -C alpine-3.17.3 -I alpine-3.17.3 -P /bin/bash -n host${Font}\n"
+    exit 0
+}
 
-while getopts m:C:I:V:P:n: option
-do
-	case "$option"
-	in
-		m) memory=$OPTARG;;
-		C) container=$OPTARG;;
-		I) image=$OPTARG;;
-		# V) volume=$OPTARG;; # Furture
-		P) program=$OPTARG;;
-		n) network=$OPTARG;;
-		\?) usage
-		    exit -2;;
-	esac
-done
+is_root() {
+    if [ 0 == $UID ]; then
+        echo -e "${OK} ${GreenBG} 当前用户是root用户，进入创建流程 ${Font}"
+        sleep 1
+    else
+        echo -e "${Error} ${RedBG} 当前用户不是root用户，请切换到root用户后重新执行脚本 ${Font}"
+        exit 1
+    fi
+}
 
-export memory=$memory
-export container=$container
-export image=$image
-# export volume=$volume # Furture
-export network=$network
-export program=$program
+# 准备容器cgroup
+prepare_container_resource_limit() {
+    echo -e "${Debug}子进程ID: $$ ${Font}"
+    echo -e "${Debug}父进程ID: $PPID ${Font}"
+    mkdir -p /sys/fs/cgroup/memory/$container
+    judge "创建内存cgroup"
+    
+    echo $$ > /sys/fs/cgroup/memory/$container/cgroup.procs
+    judge "对进程 $$ 进行资源限制"
+    
+    echo $memory > /sys/fs/cgroup/memory/$container/memory.limit_in_bytes
+    if [[ 0 -ne $? ]]; then
+        echo -e "${Error} ${RedBG} 设置限制内存大小为 ${memory} 失败${Font}"
+    fi
+}
 
-if test "$network" = host; then
-	unshare --uts --mount --pid --fork ./container.sh
+# 网络配置
+configure_network() {
+    case $network in
+        host)
+            judge "配置Host网络"
+            judge "创建名称空间"
+            prepare_container_resource_limit
+            unshare --uts --mount --pid --fork ./container.sh
+        ;;
+        none)
+            judge "配置None网络"
+            if [ -e /var/run/netns/$container ]; then
+                echo "${Error} 网络命名空间已存在: $container"
+                exit 1
+            fi
+            prepare_container_resource_limit
+            touch /var/run/netns/$container
+            unshare --uts --mount --pid --net=/var/run/netns/$container --fork ./container.sh
+        ;;
+        bridge)
+            judge "配置Bridge网络"
+            if [ -e /var/run/netns/$container ]; then
+                prepare_container_resource_limit
+                echo Abort: netns $container exists
+                exit -1
+            fi
+            
+            touch /var/run/netns/$container
+            
+            ip link add veth1-$container type veth peer name veth2-$container
+            ip link set dev veth2-$container master dockersh0
+            ip link set dev veth2-$container up
+            
+            ./set_netns.sh veth1-$container $container &
+            
+            addr=unknown
+            for i in {2..254}
+            do
+                ping -c 3 172.31.0.$i;
+                
+                if test $? != 0; then
+                    export addr=172.31.0.$i
+                    
+                    break
+                fi
+            done
+            unshare --uts --mount --pid --net=/var/run/netns/$container --fork ./container.sh
+        ;;
+        *)
+            echo "${Error} 不支持的网络模式: $network"
+            exit 1
+        ;;
+    esac
+}
 
-elif test "$network" = none; then
-	if [ -e /var/run/netns/$container ]; then
-		echo Abort: netns $container exists
-		exit -1
-	fi
 
-	touch /var/run/netns/$container
-	unshare --uts --mount --pid --net=/var/run/netns/$container --fork ./container.sh
+# 主程序
+main() {
+    export memory=""
+    export container=""
+    export image=""
+    export program=""
+    export network="bridge"
+    
+    
+    while getopts m:C:I:V:P:n: option; do
+        case $option in
+            m) export memory=$OPTARG ;;
+            C) export container=$OPTARG ;;
+            # V) export volume=$OPTARG;; # Furture
+            I) export image=$OPTARG ;;
+            P) export program=$OPTARG ;;
+            n) export network=$OPTARG ;;
+            *) export usage ;;
+        esac
+    done
+    
+    
+    
+    # 必须参数检查
+    [[ -z $container || -z $image ]] && usage
+    
+    is_root
+    configure_network
+    
+}
 
-elif test "$network" = bridge; then
-	if [ -e /var/run/netns/$container ]; then
-		echo Abort: netns $container exists
-		exit -1
-	fi
-
-	touch /var/run/netns/$container
-
-	ip link add veth1-$container type veth peer name veth2-$container
-	ip link set dev veth2-$container master dockersh0
-	ip link set dev veth2-$container up
-
-	./set_netns.sh veth1-$container $container &
-
-	addr=unknown
-	for i in {2..254}
-	do
-		ping -c 3 172.31.0.$i;
-
-		if test $? != 0; then
-			export addr=172.31.0.$i
-
-			break
-		fi
-	done
-
-	unshare --uts --mount --pid --net=/var/run/netns/$container --fork ./container.sh
-
-fi
+main $@
